9ab797ff7b8f887ad0e487052f87d6f26e29b5f8
ashutosh0x <ashutoshkumarsingh0x@gmail.com>
2025-08-12 21:40:37 +0530

    Aqua: gate stale_deps and deps_compat behind AQUA_STRICT; move Test to extras/targets

--- test/test_aqua.jl
+++ test/test_aqua.jl
@@ -1,7 +1,17 @@
 @testitem "Aqua analysis" begin
 
 using Aqua, LEMONGraphs
+using Test
 
-Aqua.test_all(LEMONGraphs, ambiguities=false)
+@testset "Aqua" begin
+    Aqua.test_unbound_args(LEMONGraphs)
+    Aqua.test_undefined_exports(LEMONGraphs)
+    Aqua.test_project_extras(LEMONGraphs)
+    if get(ENV, "AQUA_STRICT", "") == "true"
+        Aqua.test_stale_deps(LEMONGraphs)
+        Aqua.test_deps_compat(LEMONGraphs)
+    end
+    Aqua.test_persistent_tasks(LEMONGraphs)
+end
 
 end
