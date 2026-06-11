47613ce001585657b88a449bc4ecda1d956dfadb
Documenter.jl <documenter@juliadocs.github.io>
2026-06-11 20:30:43 +0000

    build based on 8c41907

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"category":"section","location":"#LEMONGraphs.jl","page":"Home","text":"LEMONGraphs.jl provides Julia wrappers for selected graph algorithms from the LEMON graph library.","title":"LEMONGraphs.jl"}]
+}
